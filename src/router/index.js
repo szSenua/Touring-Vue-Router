c010625248173f6ba38f9f602fe0ea962c0bb658
Senua <chc0089@gmail.com>
2023-03-31 20:20:08 +0200

    Nested Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
-import EventDetails from '../views/EventDetails.vue'
+import EventLayout from '../views/event/Layout.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventRegister from '../views/event/Register.vue'
+import EventEdit from '../views/event/Edit.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -12,10 +15,29 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails
+    component: EventLayout,
+    children: [
+      {
+        path: '', //toma el id del elemento padre que es el de más arriba
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      },
+    ]
   },
+ 
   {
     path: '/about',
     name: 'About',
