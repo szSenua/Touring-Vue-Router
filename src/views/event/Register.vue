6b0db38a745110f322da6a05560ab5d0634d7143
Senua <chc0089@gmail.com>
2023-04-02 15:22:05 +0200

    Programmatic Navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,22 @@
 <template>
     <p>Register for the event here</p>
+    <button @click="register">Register me</button>
   </template>
   
   <script>
   
   export default {
-    props: ['event'] //recibe el evento completo
+    props: ['event'], //recibe el evento completo
+    methods: {
+      register(){
+        //Llamada a la API
+        //Si el registro se realiza, entonces redirige a los detalles del evento
+
+        this.$router.push({ //hace navegar al usuario a esta localización (EventDetails)
+          name: 'EventDetails'
+          
+        })
+      }
+    }
   }
   </script>
