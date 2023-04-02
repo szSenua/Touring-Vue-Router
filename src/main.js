f6055a447dd744f6efb1a8c10ddbe846aa48f679
Senua <chc0089@gmail.com>
2023-04-02 16:41:00 +0200

    Flash Messages

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//crear un objeto reactivo global para implementarlo en múltiples componentes.
+const GStore = reactive({ flashMessage: '' })
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore' , GStore) // String (Key , Value) Hace que nuestro objeto esté disponble para usarlos en componentes de nuestra app (inyección)
   .mount('#app')
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -7,10 +7,17 @@
   
   export default {
     props: ['event'], //recibe el evento completo
+    inject: ['GStore'], //Hace que el objeto GStore esté disponible dentro de este componente
     methods: {
       register(){
         //Llamada a la API
         //Si el registro se realiza, entonces redirige a los detalles del evento
+
+        this.GStore.flashMessage = 'You are successfully registered for ' + this.event.title
+
+        setTimeout(() => {
+          this.GStore.flashMessage = ''
+        }, 3000) //Después de tres segundos, limpia flashMessage
 
         this.$router.push({ //hace navegar al usuario a esta localización (EventDetails)
           name: 'EventDetails'
